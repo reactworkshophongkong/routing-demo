e69d778482806f1311c1caed772287e90cd1fcfc
honchan <honkangchan@gmail.com>
2021-05-06 20:33:52 +0800

    add comments

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ export default function App() {
     marginRight: "5px"
   };
 
+    /* Todo lets extend the app
+    1) Create a new Route to serve component Users , 
+      data can be fetched from /profile
+
+    2) Extend app and display Comments within Post component
+    data can be fetched from post/:id/comments
+  */
+
   return (
     <Router>
       <div className="App">
